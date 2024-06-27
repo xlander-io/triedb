ba84ec63ff8109c692461f04d1147c23eebd2257
leo <leo@leos-MacBook-Pro.local>
2024-06-27 21:16:24 +0800

    update

--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
 module github.com/xlander-io/triedb
 
-go 1.19
+go 1.22.1
 
-require golang.org/x/crypto v0.24.0
+require (
+	github.com/xlander-io/cache v0.0.0-20240624033141-4a697c3bc4cc
+	github.com/xlander-io/kv v0.0.0-20240627123727-434d0569d2e2
+	golang.org/x/crypto v0.24.0
+)
 
 require golang.org/x/sys v0.21.0 // indirect
--- triedb.go
+++ triedb.go
@@ -1,6 +1,10 @@
 package triedb
 
 import (
+	"errors"
+
+	"github.com/xlander-io/cache"
+	"github.com/xlander-io/kv"
 	"github.com/xlander-io/triedb/util"
 )
 
@@ -8,9 +12,58 @@ import (
 //const VAL_LEN_LIMIT = 4096 * 1024 * 1024 // in bytes uint32 limit
 
 type TrieDB struct {
-	//Root_hash *util.Hash
+	root_node *Node
+	kvdb      kv.KVDB
+	cache     *cache.Cache
 }
 
-func NewTrieDB(root_hash *util.Hash) {
+func NewTrieDB(kvdb_ kv.KVDB, cache_ *cache.Cache, root_hash *util.Hash) (*TrieDB, error) {
+	if kvdb_ == nil {
+		return nil, errors.New("NewTrieDB kvdb is nil")
+	}
+
+	if cache_ == nil {
+		return nil, errors.New("NewTrieDB cache is nil")
+	}
+
+	if root_hash == nil {
+
+		return &TrieDB{
+			cache:     cache_,
+			kvdb:      kvdb_,
+			root_node: &Node{},
+		}, nil
+
+	} else {
+		trie_db := TrieDB{
+			cache:     cache_,
+			kvdb:      kvdb_,
+			root_node: &Node{node_hash: root_hash},
+		}
+
+		root_node_bytes, root_node_err := trie_db.getBytesFromKVDB(root_hash)
+		if root_node_err != nil {
+			return nil, errors.New("getBytesFromKVDB err in NewTrieDB, err: " + root_node_err.Error())
+		}
+
+		trie_db.root_node.node_bytes = root_node_bytes
+		trie_db.root_node.deserialize()
+
+		return &trie_db, nil
+	}
+}
+
+func (trie_db *TrieDB) getBytesFromKVDB(node_hash *util.Hash) ([]byte, error) {
+
+	if node_hash == nil {
+		return nil, nil
+	}
+
+	node_val, get_err := trie_db.kvdb.Get((*node_hash)[:])
+	if get_err != nil {
+		return nil, get_err
+	} else {
+		return node_val, nil
+	}
 
 }
