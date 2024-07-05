3cdd23f0f46e3dde7d06b63b6dcd55a3daa7823a
pandaxcl <pandaxcl@163.com>
2024-07-05 10:16:38 +0800

    1. first test case for triedb worked correctly:)

--- triedb_test.go
+++ triedb_test.go
@@ -6,8 +6,33 @@ import (
 
 	"github.com/xlander-io/cache"
 	"github.com/xlander-io/hash"
+	"github.com/xlander-io/kv"
 	"github.com/xlander-io/kv_leveldb"
 )
+
+func (tdb *TrieDB) testCommit() (*hash.Hash, error) {
+	rootHash, toUpdate, toDel, err := tdb.CalHash()
+	if err != nil {
+		return nil, err
+	}
+
+	b := kv.NewBatch()
+	for hex_string, update_v := range toUpdate {
+		b.Put([]byte(hex_string), update_v)
+	}
+
+	for _, del_v := range toDel {
+		b.Delete(del_v.Bytes())
+	}
+
+	err = tdb.kvdb.WriteBatch(b, true)
+
+	if nil != err {
+		return nil, err
+	}
+
+	return rootHash, err
+}
 
 func TestSimple(t *testing.T) {
 
@@ -22,29 +47,43 @@ func TestSimple(t *testing.T) {
 			Commit_thread_limit: 1,
 		})
 
-		tdb.Update([]byte("1"), []byte([]byte("val_1")))
-		tdb.Update([]byte("12"), []byte([]byte("val_12")))
-		tdb.Update([]byte("13"), []byte([]byte("val_13")))
-		tdb.Update([]byte("14"), []byte([]byte("val_14")))
-		tdb.Update([]byte("123"), []byte([]byte("val_123")))
-		tdb.Update([]byte("1234"), []byte([]byte("val_1234")))
+		tdb.Update([]byte("1"), []byte("val_1"))
+		tdb.Update([]byte("12"), []byte("val_12"))
+		tdb.Update([]byte("13"), []byte("val_13"))
+		tdb.Update([]byte("14"), []byte("val_14"))
+		tdb.Update([]byte("123"), []byte("val_123"))
+		tdb.Update([]byte("1234"), []byte("val_1234"))
 
-		rootHash, _, _, _ = tdb.CalHash()
-		//tdb.GenDotFile("./trie_test_simple.dot", false)
+		_rootHash, err := tdb.testCommit()
+
+		if nil != err {
+			t.Fatal(err)
+		}
+
+		rootHash = _rootHash
 
 		kvdb.Close()
 	}
 
 	{
-		kvdb, _ := kv_leveldb.NewDB(db_path)
+		kvdb, err := kv_leveldb.NewDB(db_path)
+		if nil != err {
+			t.Fatal(err)
+		}
 		//
-		c, _ := cache.New(nil)
-		tdb, _ := NewTrieDB(kvdb, c, &TrieDBConfig{
+		c, err := cache.New(nil)
+		if nil != err {
+			t.Fatal(err)
+		}
+		tdb, err := NewTrieDB(kvdb, c, &TrieDBConfig{
 			Root_hash:           rootHash.Clone(),
 			Commit_thread_limit: 1,
 		})
+		if nil != err {
+			t.Fatal(err)
+		}
 
-		err := tdb.Update([]byte("2"), []byte([]byte("val_1")))
+		err = tdb.Update([]byte("2"), []byte("val_1"))
 		if nil != err {
 			t.Fatal(err)
 		}
