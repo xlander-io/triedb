2b506862148400c1946870cacca3a8a5f61a4d9c
pandaxcl <pandaxcl@163.com>
2024-07-06 15:11:56 +0800

    1. update triedb_test.go to conforms to test case mainworkflow

--- example/main.go
+++ example/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/xlander-io/cache"
+	"github.com/xlander-io/hash"
 	"github.com/xlander-io/kv"
 	"github.com/xlander-io/kv_leveldb"
 	"github.com/xlander-io/triedb"
@@ -30,60 +31,78 @@ func main() {
 
 	const db_path = "./kv_leveldb_test.db"
 	os.RemoveAll(db_path)
-	kvdb, _ := kv_leveldb.NewDB(db_path)
-	//
-	c, _ := cache.New(nil)
-	tdb, _ := triedb.NewTrieDB(kvdb, c, &triedb.TrieDBConfig{
-		Commit_thread_limit: 1,
-	})
 
-	tdb.Update([]byte("1"), []byte("val_1"))
-	tdb.Update([]byte("12"), []byte("val_12"))
-	tdb.Update([]byte("13"), []byte("val_13"))
-	tdb.Update([]byte("14"), []byte("val_14"))
-	tdb.Update([]byte("123"), []byte("val_123"))
-	tdb.Update([]byte("1234"), []byte("val_1234"))
+	var rootHash *hash.Hash
 
-	tdb.GenDotFile("./test_pre.dot", false)
+	{
+		kvdb, _ := kv_leveldb.NewDB(db_path)
+		//
+		c, _ := cache.New(nil)
+		tdb, _ := triedb.NewTrieDB(kvdb, c, &triedb.TrieDBConfig{
+			Root_hash:           nil,
+			Commit_thread_limit: 1,
+		})
 
-	root_hash, to_update, to_del, cal_herr := tdb.CalHash()
-	if cal_herr != nil {
-		fmt.Println("tdb.CalHash() err:", cal_herr.Error())
-		return
+		tdb.Update([]byte("1"), []byte("val_1"))
+		tdb.Update([]byte("12"), []byte("val_12"))
+		tdb.Update([]byte("13"), []byte("val_13"))
+		tdb.Update([]byte("14"), []byte("val_14"))
+		tdb.Update([]byte("123"), []byte("val_123"))
+		tdb.Update([]byte("1234"), []byte("val_1234"))
+
+		tdb.GenDotFile("./test_pre.dot", false)
+
+		root_hash, to_update, to_del, cal_herr := tdb.CalHash()
+		if cal_herr != nil {
+			fmt.Println("tdb.CalHash() err:", cal_herr.Error())
+			return
+		}
+
+		tdb.GenDotFile("./test_after_hash.dot", false)
+
+		fmt.Println("root_hash:", fmt.Sprintf("%x", root_hash.Bytes()))
+
+		// fmt.Println("len(to_update):", len(to_update))
+
+		b := kv.NewBatch()
+		for hex_string, update_v := range to_update {
+			b.Put([]byte(hex_string), update_v)
+			fmt.Println("hex_string ", fmt.Sprintf("%x", hex_string))
+		}
+
+		for _, del_v := range to_del {
+			b.Delete(del_v.Bytes())
+		}
+
+		write_err := kvdb.WriteBatch(b, true)
+
+		if write_err != nil {
+			fmt.Println("write_err:", write_err)
+		}
+
+		rootHash = root_hash
+		kvdb.Close()
 	}
 
-	tdb.GenDotFile("./test_after_hash.dot", false)
+	{
+		kvdb, _ := kv_leveldb.NewDB(db_path)
+		//
+		c, _ := cache.New(nil)
 
-	fmt.Println("root_hash:", fmt.Sprintf("%x", root_hash.Bytes()))
+		tdb2, _ := triedb.NewTrieDB(kvdb, c, &triedb.TrieDBConfig{
+			Root_hash:           rootHash,
+			Commit_thread_limit: 1,
+		})
 
-	// fmt.Println("len(to_update):", len(to_update))
+		tdb2.Update([]byte("2"), []byte("val_2"))
+		tdb2.Update([]byte("1a"), []byte([]byte("val_1a")))
+		tdb2.Update([]byte("12a"), []byte([]byte("val_12a")))
+		tdb2.Update([]byte("123a"), []byte([]byte("val_123a")))
 
-	b := kv.NewBatch()
-	for hex_string, update_v := range to_update {
-		b.Put([]byte(hex_string), update_v)
-		fmt.Println("hex_string ", fmt.Sprintf("%x", hex_string))
+		tdb2.GenDotFile("./read_from_kvdb.dot", false)
+
+		kvdb.Close()
 	}
-
-	for _, del_v := range to_del {
-		b.Delete(del_v.Bytes())
-	}
-
-	write_err := kvdb.WriteBatch(b, true)
-
-	if write_err != nil {
-		fmt.Println("write_err:", write_err)
-	}
-
-	tdb2, _ := triedb.NewTrieDB(kvdb, c, &triedb.TrieDBConfig{
-		Commit_thread_limit: 1,
-	})
-
-	tdb2.Update([]byte("2"), []byte("val_2"))
-	tdb2.Update([]byte("1a"), []byte([]byte("val_1a")))
-	tdb2.Update([]byte("12a"), []byte([]byte("val_12a")))
-	tdb2.Update([]byte("123a"), []byte([]byte("val_123a")))
-
-	tdb2.GenDotFile("./read_from_kvdb.dot", false)
 
 	// ///////
 	// c2, _ := cache.New(nil)
